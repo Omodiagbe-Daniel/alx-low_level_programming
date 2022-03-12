5594b2ee8f96d2420373b6b1aff47a6b474d426d
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-03-11 20:08:49 -0800

    printing a-z excluding q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,11 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		if (c == 'q' || c == 'e')
-			continue;
-		putchar(c);
-
+{
+	if (c == 'q' || c == 'e')
+		continue;
+	putchar(c);
+}
 	putchar(10);
 	return (0);
 }
