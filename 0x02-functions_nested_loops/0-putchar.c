fb7ae63b9fae4be23ac4bde156803042da191648
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-03-15 09:50:16 -0700

    printing _putchar using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /*
+ * main.h - header file
  *Description:codes start and ends here
  * main - Entry point
  * Return: Always 0 (Success)
@@ -14,8 +15,9 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 7; n++)
-{
+	{
 	putchar(alp[n]);
+	}
 	putchar('\n');
-}
+	return (0);
 }
