bd666e44daed7fbe4cfbea41e7e18aaba8179488
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-03-15 17:23:51 -0700

    printing out arithmetic symbols

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return (1);
 putchar(43);
+return (1);
 }
 else if (n == 0)
 {
-return (0);
 putchar(48);
+return (0);
 }
 else
 {
-return (-1);
 putchar(45);
+return (-1);
 }
 }
