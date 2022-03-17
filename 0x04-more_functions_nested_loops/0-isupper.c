9612bc43c91003e4a72e31cc0966c6251b793eb9
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-03-17 11:35:41 -0700

    function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,14 @@
 #include <std.io>
 
 /**
- * _isupper: Returns 1 for uppercase alphabets and 0 for lowercase alphabets
- * @c : of type int
- * Return : Always 0 (Success)
+ * _isupper - checks for upper case alphabets
+ * @c: letter being tested
+ * Return: int Always 0 (Success)
  */
 int _isupper(int c)
 {
-c = 65;
-while (c <= 90)
-{
-return (1);
+if (c >= 65 && c <= 90)
+	return (1);
 else
 	return (0);
-}
-_putchar(10);
-c++
 }
