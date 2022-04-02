12a95ad152fa21e5b7f34203a08c66376ab00301
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-04-02 02:58:05 +0000

    program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
-* main - add arguments together
-* @argc: counts the number of argument
-* @argv: print the argument
-* Return: Always (0)
-*/
-
+ * main - adds positive numbers
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-int i = 0;
-int sum = 0;
+	unsigned int i, sum, num;
 
-if (atoi(argv[i]) != (i <= 9))
-{
-printf("%d\n", 0);
-}
-for (i = 0; i < argc; i++)
-{
-if (atoi(argv[i]) != (0 <= 9))
-{
-printf("%s\n", "Error");
-return (1);
-}
-else if (atoi(argv[i]) == (i <= 9))
-{
-sum += atoi(argv[i]);
-printf("%d\n", sum);
-}
-}
-return (0);
+	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (sum);
 }
