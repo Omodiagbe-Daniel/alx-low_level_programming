2ecbe997d4d2d04cafef0321c6b7e90c02a82d83
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-04-08 03:57:25 +0000

    function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
  * _calloc - allocates memory for an array using malloc
  * @nmemb: n elements
  * @size: bytes
