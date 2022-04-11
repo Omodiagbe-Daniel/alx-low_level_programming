3481632a272a21261b407704e39b9159a9e19f98
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-04-11 19:27:28 +0000

    commiting readme

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-int n;
-int i;
+	int n;
+	int i;
 
-for (n = 48; n <= 57; n++)
-{
-for (i = 48; i <= 57; i++)
-{
-putchar(n);
-putchar(i);
-putchar(',');
-putchar(' ');
-}
-}
-putchar(10);
-return (0);
+	for (n = 48; n <= 57; n++)
+	{
+		for (i = 49; i <= 57; i++)
+		{
+			putchar(n);
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(10);
+	return (0);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef _DOG_H_
+#define _DOG_H_
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,3 +16,4 @@ struct dog
 	float age;
 	char *owner;
 };
+#endif
