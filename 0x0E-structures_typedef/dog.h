e45b2d33cd97bee7d4888d8d178e18d7bf30d999
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-04-11 18:59:25 +0000

    example on how to define a type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct dog - example of a struct combining different elements
+* @name: first member
+* @age: second member
+* @owner: third member
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
