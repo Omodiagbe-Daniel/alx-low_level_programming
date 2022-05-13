33a44e751411954154187e24609bf3226a8bc933
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-05-13 23:56:52 +0000

    functions that contanates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i = 0, j = 0;
+	int i = 0, j = 0;
 
-        while (dest[j] != '\0')
-        {
-                j++;
-        }
-        while (src[i] != '\0' && i < n)
-        {
-                dest[j] = src[i];
-                i++;
-                j++;
-        }
-        return (dest);
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while (src[i] != '\0' && i < n)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	return (dest);
 }
 
 
