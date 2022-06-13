29868a04d62fa508d625256acc9543b22f82fcb2
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-06-13 15:34:14 +0000

    dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check for lower case alphabets and upper  case alphabets
+ * @c : letter being tested
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 91))
+		return (1);
+	else
+		return (0);
+}
