6e58179307a1d3609d4a79169fbafddeb05bd8fe
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-18 18:32:42 +0100

    added main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H_
 #include <stdio.h>
 void positive_or_negative(int i);
-#endif /* MAIN_H */
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
+#endif
