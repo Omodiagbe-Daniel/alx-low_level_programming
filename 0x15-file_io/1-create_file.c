a976d6a61107c62a3f7c8f21f7c33ac20d87122f
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-20 19:31:03 +0100

    create a file using O_CREAT

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @file: pointer to filename
+* @text_content: content of the file
+* Return: 1 on success, -1
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int create, fd, num;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		return (-1);
+	}
+	num = 0;
+	while (text_content[num])
+	{
+		num++;
+	}
+	create = write(fd, text_content, num);
+	if (create != num)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
